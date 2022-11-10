8568951fd8f4393afa4a1f52ddde945d64e90105
konradrudnicki <konradrudnicki0@gmail.com>
2022-11-10 20:48:00 +0100

    Services creations

---
+++ pet-clinic-data/src/main/java/konradrudnicki/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package konradrudnicki.springframework.sfgpetclinic.services;
+
+import konradrudnicki.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
