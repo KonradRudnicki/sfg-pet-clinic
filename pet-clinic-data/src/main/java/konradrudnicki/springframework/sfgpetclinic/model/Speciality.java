817361944061331a9f47a16f817968b9004529f7
konradrudnicki <konradrudnicki0@gmail.com>
2022-11-29 21:33:09 +0100

    Add Vet Speciality

---
+++ pet-clinic-data/src/main/java/konradrudnicki/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package konradrudnicki.springframework.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
