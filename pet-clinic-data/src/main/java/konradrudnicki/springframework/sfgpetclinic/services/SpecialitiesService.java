12ad2e244a57bf7cb9b857195ea338feb268f360
konradrudnicki <konradrudnicki0@gmail.com>
2022-11-29 23:52:35 +0100

    Create Speciality Map Service

---
+++ pet-clinic-data/src/main/java/konradrudnicki/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package konradrudnicki.springframework.sfgpetclinic.services;
+
+import konradrudnicki.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
