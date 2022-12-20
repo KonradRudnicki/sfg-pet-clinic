f42ae9f4cfed0b8572de76a0becfe205df47dbe6
konradrudnicki <konradrudnicki0@gmail.com>
2022-12-20 19:39:05 +0100

    Add Spring Data JPA Visit Service

--- pet-clinic-web/src/main/java/konradrudnicki/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/konradrudnicki/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package konradrudnicki.springframework.sfgpetclinic.bootstrap;
 
 import konradrudnicki.springframework.sfgpetclinic.model.*;
-import konradrudnicki.springframework.sfgpetclinic.services.OwnerService;
-import konradrudnicki.springframework.sfgpetclinic.services.PetTypeService;
-import konradrudnicki.springframework.sfgpetclinic.services.SpecialityService;
-import konradrudnicki.springframework.sfgpetclinic.services.VetService;
+import konradrudnicki.springframework.sfgpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                      SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -86,6 +86,13 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner2);
 
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
+
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
@@ -103,5 +110,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
+
+
     }
 }
