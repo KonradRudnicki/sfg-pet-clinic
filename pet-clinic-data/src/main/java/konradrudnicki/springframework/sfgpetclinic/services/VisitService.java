2804c9cfb1292e8561e54f4df3774aa5e0615e8f
konradrudnicki <konradrudnicki0@gmail.com>
2022-12-20 19:22:42 +0100

    Add Map Based Visit Service

---
+++ pet-clinic-data/src/main/java/konradrudnicki/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package konradrudnicki.springframework.sfgpetclinic.services;
+
+import konradrudnicki.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
